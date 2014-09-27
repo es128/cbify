48b052debf2682e24ba5efb123920bd64372f931
Elan Shanker <elan.shanker@gmail.com>
2014-09-27 10:43:06 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var fnArgs = require('fn-args');
+
+module.exports = function callbackify (fn) {
+	if (typeof fn !== 'function') {
+		throw new TypeError('Expected a function');
+	}
+
+	var origArgs = fnArgs(fn);
+	var lastArg = origArgs[origArgs.length - 1];
+
+	if (lastArg.toLowerCase().indexOf('callback') !== -1 || lastArg === 'cb') {
+		return fn;
+	}
+
+	return function () {
+		var argsL = arguments.length;
+		var args = argsL > 1 ? [].slice.call(arguments, 0, argsL - 1) : [];
+		var cb = arguments[argsL - 1];
+
+		if (typeof cb !== 'function') {
+			throw new Error('Must pass callback function');
+		}
+
+		try {
+			cb(null, fn.apply(this, args));
+		} catch (_err) {
+			cb(_err);
+		}
+	};
+};
