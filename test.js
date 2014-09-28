9dd8d2dea8883f1bfc035f27eea816f0e62f828b
Elan Shanker <elan.shanker@gmail.com>
2014-09-28 10:25:42 -0400

    Test type checking of wrapped function

--- test.js
+++ test.js
@@ -2,8 +2,9 @@
 var assert = require('assert');
 var callbackify = require('./');
 
+function sum (a, b) { return a + b; }
+
 it('should make a sync function pass return to a callback', function () {
-	function sum (a, b) { return a + b; }
 	var sumCb = callbackify(sum);
 	var answer;
 	sumCb(32, 96, function (err, result) { answer = result; });
@@ -12,5 +13,10 @@ it('should make a sync function pass return to a callback', function () {
 
 it('should throw if not given a function', function () {
 	assert.throws(function () { callbackify(''); });
+});
+
+it('should throw if wrapped function not given a callback', function () {
+	var sumCb = callbackify(sum);
+	assert.throws(function () { sumCb(32, 96); });
 });
 
